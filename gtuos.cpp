a964bf872a321aadc924d16055955a2f8ed5ccb8
CSE312Ubuntu <hcbayram@gtu.edu.tr>
2019-04-02 15:29:43 -0700

    Cpp side is made beautiful

--- gtuos.cpp
+++ gtuos.cpp
@@ -17,8 +17,8 @@ GTUOS::GTUOS() {
 
 
 GTUOS::~GTUOS() {
-//	inFile.close();
-//	oFile.close();
+	inFile.close();
+	oFile.close();
 }
 
 
@@ -31,7 +31,7 @@ uint64_t GTUOS::load_exec(CPU8080 & cpu) {
     for (i = 0; (int)cpu.memory->at(address_bc + i) != 0; ++i) 
         file_name[i] = (char) cpu.memory->at(address_bc + i);
     file_name[i] = '\0';
-    printf("file name : %s, address hl : %x\n", file_name, address_hl);
+    //printf("file name : %s, address hl : %x\n", file_name, address_hl);
     cpu.ReadFileIntoMemoryAt(file_name, address_hl);    
     return this->LOAD_EXEC.cycle;
 }
@@ -39,34 +39,23 @@ uint64_t GTUOS::load_exec(CPU8080 & cpu) {
 
 
 uint64_t GTUOS::process_exit(CPU8080 & cpu) {
-    printf("\n\n\n");
     uint16_t proc_cur = ((Memory *)(cpu.memory))->getBaseRegister();
     uint16_t proc_start = 20000;
     uint16_t scheduler_addr = 0x04E8;
     uint16_t table_start, table_cur;
     table_start = table_cur = 0x2710;
-    printf("%x : %d | %x : %d | %x : %d\n", 0x2710, cpu.memory->physicalAt(0x2710), 0x2910, cpu.memory->physicalAt(0x2910), 0x2B10, cpu.memory->physicalAt(0x2B10));
-    //printf("%x : %d\n", table_cur, cpu.memory->physicalAt(table_cur));
+    //printf("%x : %d | %x : %d | %x : %d\n", 0x2710, cpu.memory->physicalAt(0x2710), 0x2910, cpu.memory->physicalAt(0x2910), 0x2B10, cpu.memory->physicalAt(0x2B10));
     int k = 0; 
     for (uint16_t i = proc_cur; proc_start != i; i -= 5000)
         ++k;
     for (int i = 0; i < k; ++i)
        table_cur += 0x0200;
     cpu.memory->physicalAt(table_cur) = 0; // DONE
-    printf("cur : %x\n", table_cur);
     //printf("%x : %d\n", table_cur, cpu.memory->physicalAt(table_cur));
-        printf("%x : %d | %x : %d | %x : %d\n", 0x2710, cpu.memory->physicalAt(0x2710), 0x2910, cpu.memory->physicalAt(0x2910), 0x2B10, cpu.memory->physicalAt(0x2B10));
+    //printf("%x : %d | %x : %d | %x : %d\n", 0x2710, cpu.memory->physicalAt(0x2710), 0x2910, cpu.memory->physicalAt(0x2910), 0x2B10, cpu.memory->physicalAt(0x2B10));
     ((Memory *)(cpu.memory))->setBaseRegister(0);
     cpu.state->pc = scheduler_addr;
-    printf("\n\n\n");
     return this->PROCESS_EXIT.cycle;
-}
-
-
-void GTUOS::write_pc(CPU8080 & cpu) {
-    uint16_t proc_cur = ((Memory *)(cpu.memory))->getBaseRegister();
-    uint16_t pc = cpu.state->pc; 
-    printf("Process address : %x %x\n", proc_cur, pc);
 }
 
 
@@ -75,8 +64,6 @@ void GTUOS::write_pc(CPU8080 & cpu) {
 uint64_t GTUOS::handleCall(CPU8080 & cpu){
 	uint8_t content_A = cpu.state->a; // So as to understand which system call to be executed, register A is checked. System call number is assigned to register A.
 	uint64_t cycle = 0; // # of cycles during any of the system calls below is assigned to this variable so that it can be returned.
-
-    
 
 	if (content_A == PRINT_B.number)
 		cycle = call_print_b(cpu);
@@ -94,9 +81,6 @@ uint64_t GTUOS::handleCall(CPU8080 & cpu){
         cycle = load_exec(cpu);
     else if (content_A == PROCESS_EXIT.number)
     	cycle = process_exit(cpu);
-    //
-    else if (content_A == WRITE_PC.number)
-        write_pc(cpu);
 	
 	return cycle;
 }
@@ -105,8 +89,8 @@ uint64_t GTUOS::handleCall(CPU8080 & cpu){
 
 // Calls PRINT_B system call. Prints the contents of register B the screen as decimal.
 uint64_t GTUOS::call_print_b(const CPU8080 & cpu) {
-	//oFile << int(cpu.state->b) << " ";	
-    cout << int(cpu.state->b) << " ";
+	oFile << int(cpu.state->b) << " ";	
+    //cout << int(cpu.state->b) << " ";
 	return this->PRINT_B.cycle;
 }
 
@@ -115,8 +99,8 @@ uint64_t GTUOS::call_print_b(const CPU8080 & cpu) {
 // Calls PRINT_MEM system call. Prints the of contents of memory pointed by register  B and register C as decimal.
 uint64_t GTUOS::call_print_mem(const CPU8080 & cpu) {
 	uint16_t address = (((uint16_t)cpu.state->b) << 8) | cpu.state->c;
-	//oFile << (int)cpu.memory->at(address) << endl;	
-    cout << (int)cpu.memory->at(address) << endl;
+	oFile << (int)cpu.memory->at(address) << endl;	
+    //cout << (int)cpu.memory->at(address) << endl;
 	return this->PRINT_MEM.cycle;
 }
 
@@ -126,13 +110,11 @@ uint64_t GTUOS::call_print_mem(const CPU8080 & cpu) {
 uint64_t GTUOS::call_print_str(const CPU8080 & cpu) {
 	uint16_t address = (((uint16_t)cpu.state->b) << 8) | cpu.state->c;
 	int local_cycle_num = 0;
-    //printf("\n\n\n\n\n\nprompt mesajin basladigi adres : %x\n\n\n\n\n\n", address);
     for(uint16_t i = address; cpu.memory->at(i) != '\0';  ++i){
-        //oFile << char(cpu.memory->at(i));
-        cout << char(cpu.memory->at(i));
+        oFile << char(cpu.memory->at(i));
+        //cout << char(cpu.memory->at(i));
 		local_cycle_num++;
     }	
-    //cout << endl << endl << endl << endl;
 	return this->PRINT_STR.cycle * local_cycle_num;
 }
 
@@ -142,8 +124,8 @@ uint64_t GTUOS::call_print_str(const CPU8080 & cpu) {
 uint64_t GTUOS::call_read_b(const CPU8080 & cpu) {
 	int content;
 	cout << "Enter the number that is written to register B ranging from 0 to 255 : ";
-	//inFile >> content;
-    cin >> content;
+	inFile >> content;
+    //cin >> content;
 	if ((content < 0) || (content > 255)) {
 		cout << "The number isn't in the valid range. 0 is assigned." << endl;
 		content = 0;
@@ -158,8 +140,8 @@ uint64_t GTUOS::call_read_b(const CPU8080 & cpu) {
 uint64_t GTUOS::call_read_mem(const CPU8080 & cpu) {
 	uint8_t content;
 	cout << "Enter the number that is written to the memory address pointed by register B and register C : ";
-	//inFile >> content;
-    cin >> content;
+	inFile >> content;
+    //cin >> content;
 	if ((content < 0) || (content > 255)) {
 		cout << "The number isn't in the valid range. 0 is assigned." << endl;
 		content = 0;
@@ -175,12 +157,9 @@ uint64_t GTUOS::call_read_mem(const CPU8080 & cpu) {
 uint64_t GTUOS::call_read_str(const CPU8080 & cpu) {
 	string str = "";
 	cout << "Enter a string that will be written into the address pointed by register B anc register C : ";
-	//getline(inFile, str);
-    //getline(cin, str);
-    cin >> str;
-	//cout << str << endl;
+	getline(inFile, str);
+    //cin >> str;
 	uint16_t address = (((uint16_t)cpu.state->b) << 8) | cpu.state->c;
-	//cout << (int)cpu.state->b << " " << (int)cpu.state->c << endl;
 	int k = address;
 	int local_cycle_num = 0;
 	for (size_t i = 0; i < str.length(); ++k, ++i) {
---
+++ gtuos.h
@@ -0,0 +1,48 @@
+#ifndef H_GTUOS
+#define H_GTUOS
+
+#include "8080emuCPP.h"
+#include <fstream>
+#include <ostream>
+
+using namespace std;
+
+class GTUOS{
+	public:
+		GTUOS();
+		~GTUOS();
+		uint64_t handleCall(CPU8080 & cpu);
+		uint64_t call_print_b(const CPU8080 & cpu);
+		uint64_t call_print_mem(const CPU8080 & cpu);
+		uint64_t call_print_str(const CPU8080 & cpu);
+		uint64_t call_read_b(const CPU8080 & cpu);
+		uint64_t call_read_mem(const CPU8080 & cpu);
+		uint64_t call_read_str(const CPU8080 & cpu);
+        uint64_t load_exec(CPU8080 & cpu);
+        uint64_t process_exit(CPU8080 & cpu);
+
+	private:
+		ofstream oFile;
+		ifstream inFile;
+		class syscall {
+				public:
+					syscall(int n, int c) {
+						number = n;
+						cycle = c;
+					}
+			
+					int number;
+					uint64_t cycle;
+		};
+		syscall PRINT_B = syscall(4, 10);
+		syscall PRINT_MEM = syscall(3, 10);
+		syscall READ_B = syscall(7, 10);
+		syscall READ_MEM = syscall(2, 10);
+		syscall PRINT_STR = syscall(1, 10); // 10 cycles per character
+		syscall READ_STR = syscall(8, 10); // 10 cycles per character
+        syscall LOAD_EXEC = syscall(5, 100);
+        syscall PROCESS_EXIT = syscall(9, 80);
+
+};
+
+#endif
